eee8cbe37657bdad7a23e1294192eeff2ad9a508
Roman Shapiro <netframeworker@gmail.com>
2019-12-10 16:40:15 +0700

    Added Web Designer Angular sample

---
+++ WebDesignerSamples/WebDesigner_Angular(Core)/ClientApp/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'create', component: HomeComponent },
+      { path: 'edit/:id', component: HomeComponent },
+      { path: 'preview/:id', component: HomeComponent },
+      { path: '**', redirectTo: '' },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ WebDesignerSamples/WebDesigner_Angular(Core)/Startup.cs
@@ -0,0 +1,81 @@
+using GrapeCity.ActiveReports.Aspnetcore.Designer;
+using GrapeCity.ActiveReports.Aspnetcore.Viewer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using System.IO;
+using WebDesignerAngularCore.Implementation;
+using WebDesignerAngularCore.Services;
+
+namespace WebDesignerAngularCore
+{
+	public class Startup
+	{
+		private static readonly DirectoryInfo ResourcesRootDirectory = new DirectoryInfo(@".\resources\");
+		private static readonly DirectoryInfo TemplatesRootDirectory = new DirectoryInfo(@".\templates\");
+		private static readonly DirectoryInfo DataSetsRootDirectory = new DirectoryInfo(@".\datasets\");
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services
+				.AddReporting()
+				.AddDesigner()
+				.AddSingleton<ITemplatesService>(new FileSystemTemplates(TemplatesRootDirectory))
+				.AddSingleton<IDataSetsService>(new FileSystemDataSets(DataSetsRootDirectory))
+				.AddMvc()
+				.AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+			// In production, the Angular files will be served from this directory
+			services.AddSpaStaticFiles(configuration =>
+			{
+				configuration.RootPath = "ClientApp/dist";
+			});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseReporting(config => config.UseFileStore(ResourcesRootDirectory));
+			app.UseDesigner(config => config.UseFileStore(ResourcesRootDirectory, false));
+
+			app.UseStaticFiles();
+			app.UseSpaStaticFiles();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller}/{action=Index}/{id?}");
+			});
+
+			app.UseSpa(spa =>
+			{
+				// To learn more about options for serving an Angular SPA from ASP.NET Core,
+				// see https://go.microsoft.com/fwlink/?linkid=864501
+
+				spa.Options.SourcePath = "ClientApp";
+
+				if (env.IsDevelopment())
+				{
+					spa.UseAngularCliServer(npmScript: "start");
+				}
+			});
+		}
+	}
+}
