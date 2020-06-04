aa57b4629a7f817e8b51ea00f56b7fbe37d4615d
Sergey Abakumov <Sabakumov@binck.nl>
2020-06-04 12:51:40 +0200

    initial commit for the new sample

---
+++ JsViewerSamples/JSViewer_Blazor/Data/ReportsService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSViewer_Blazor.Data
+{
+    public class ReportsService
+    {
+        public static string EmbeddedReportsPrefix = "JSViewer_Blazor.Reports";
+        public IEnumerable<string> GetReports()
+        {
+            string[] validExtensions = { ".rdl", ".rdlx", ".rdlx-master" };
+            return GetEmbeddedReports(validExtensions);
+
+        }
+        private static string[] GetEmbeddedReports(string[] validExtensions) =>
+            typeof(ReportsService).Assembly.GetManifestResourceNames()
+                .Where(x => x.StartsWith(EmbeddedReportsPrefix))
+                .Where(x => validExtensions.Any(x.EndsWith))
+                .Select(x => x.Substring(EmbeddedReportsPrefix.Length + 1))
+                .ToArray();
+
+    }
+}
